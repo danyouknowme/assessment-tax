7780ef8fa1d4391135108dc491a8169d45f4abd1
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-25 16:16:08 +0700

    create tax calculation request type

---
+++ tax/tax.go
@@ -0,0 +1,12 @@
+package tax
+
+type TaxCalculationRequest struct {
+	TotalIncome float64     `json:"totalIncome"`
+	Wht         float64     `json:"wht"`
+	Allowances  []Allowance `json:"allowances"`
+}
+
+type Allowance struct {
+	AllowanceType string  `json:"allowanceType"`
+	Amount        float64 `json:"amount"`
+}
