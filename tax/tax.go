ecd989839c2f7c5b7e9b69877cb6a876e2a82284
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-25 20:04:57 +0700

    fix tax calculation function

--- tax/tax.go
+++ tax/tax.go
@@ -1,5 +1,9 @@
 package tax
 
+import (
+	"math"
+)
+
 type TaxCalculationRequest struct {
 	TotalIncome float64     `json:"totalIncome"`
 	Wht         float64     `json:"wht"`
@@ -13,13 +17,37 @@ type Allowance struct {
 
 const PersonalAllowance = 60000.0
 
-func Calculate(req TaxCalculationRequest) float64 {
-	income := req.TotalIncome - PersonalAllowance
-	if income <= 150000 {
-		return 0
-	} else if income <= 500000 {
-		return (income - 150000) * 0.1
-	}
+type TaxBracket struct {
+	MinTotalIncome float64
+	MaxTotalIncome float64
+	TaxRate        float64
+}
 
-	return 0
+var taxBrackets = []TaxBracket{
+	{MinTotalIncome: 0, MaxTotalIncome: 150000, TaxRate: 0},
+	{MinTotalIncome: 150000, MaxTotalIncome: 500000, TaxRate: 0.1},
+	{MinTotalIncome: 500000, MaxTotalIncome: 1000000, TaxRate: 0.15},
+	{MinTotalIncome: 1000000, MaxTotalIncome: 2000000, TaxRate: 0.2},
+	{MinTotalIncome: 2000000, MaxTotalIncome: math.MaxFloat64, TaxRate: 0.35},
+}
+
+func Calculate(netIncome float64) float64 {
+	var tax float64 = 0
+	taxableIncome := netIncome - PersonalAllowance
+	for _, bracket := range taxBrackets {
+		if taxableIncome <= 0 {
+			break
+		}
+
+		bracketRange := bracket.MaxTotalIncome - bracket.MinTotalIncome
+		incomeInBracket := math.Min(taxableIncome, bracketRange)
+		taxInBracket := incomeInBracket * bracket.TaxRate
+		tax += taxInBracket
+		taxableIncome -= incomeInBracket
+	}
+	return formatCalculatedTax(tax)
+}
+
+func formatCalculatedTax(tax float64) float64 {
+	return math.Round(tax*100) / 100
 }
