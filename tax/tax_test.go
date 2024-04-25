a0231465a56bea6d4cc47e048865781b4ec88a6e
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-25 20:04:26 +0700

    add more test cases

--- tax/tax_test.go
+++ tax/tax_test.go
@@ -42,10 +42,10 @@ func TestCalculateTaxWithTotalIncomeOnly(t *testing.T) {
 				Wht:         0.0,
 				Allowances:  []Allowance{},
 			},
-			expect: 0,
+			expect: 0.0,
 		},
 		{
-			name: "Calculate tax with total income 500000.0",
+			name: "Total income 500,000 should return 29,000",
 			input: TaxCalculationRequest{
 				TotalIncome: 500000.0,
 				Wht:         0.0,
@@ -55,11 +55,68 @@ func TestCalculateTaxWithTotalIncomeOnly(t *testing.T) {
 			},
 			expect: 29000.0,
 		},
+		{
+			name: "Total income 500,001 should return 29,000.1",
+			input: TaxCalculationRequest{
+				TotalIncome: 500001.0,
+				Wht:         0.0,
+				Allowances: []Allowance{
+					{AllowanceType: "donation", Amount: 0.00},
+				},
+			},
+			expect: 29000.1,
+		},
+		{
+			name: "Total income 1,000,000 should return 101,000",
+			input: TaxCalculationRequest{
+				TotalIncome: 1000000.0,
+				Wht:         0.0,
+				Allowances:  []Allowance{},
+			},
+			expect: 101000.0,
+		},
+		{
+			name: "Total income 1,000,001 should return 101,000.15",
+			input: TaxCalculationRequest{
+				TotalIncome: 1000001.0,
+				Wht:         0.0,
+				Allowances:  []Allowance{},
+			},
+			expect: 101000.15,
+		},
+		{
+			name: "Total income 2,000,000 should return 298,000",
+			input: TaxCalculationRequest{
+				TotalIncome: 2000000.0,
+				Wht:         0.0,
+				Allowances:  []Allowance{},
+			},
+			expect: 298000.0,
+		},
+		{
+			name: "Total income 2,000,001 should return 298,000.2",
+			input: TaxCalculationRequest{
+				TotalIncome: 2000001.0,
+				Wht:         0.0,
+				Allowances:  []Allowance{},
+			},
+			expect: 298000.2,
+		},
+		{
+			name: "Total income 4,000,000 should return 989,000",
+			input: TaxCalculationRequest{
+				TotalIncome: 4000000.0,
+				Wht:         0.0,
+				Allowances:  []Allowance{},
+			},
+			expect: 989000.0,
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got := Calculate(tc.input)
+			totalIncome := tc.input.TotalIncome
+			got := Calculate(totalIncome)
 
 			if got != tc.expect {
 				t.Errorf("Expected %v, got %v", tc.expect, got)
