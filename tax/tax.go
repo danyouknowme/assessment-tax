e36461abb773cc586d7f6cad93395a23c62ead45
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-25 18:18:55 +0700

    implement calculate tax function

--- tax/tax.go
+++ tax/tax.go
@@ -10,3 +10,16 @@ type Allowance struct {
 	AllowanceType string  `json:"allowanceType"`
 	Amount        float64 `json:"amount"`
 }
+
+const PersonalAllowance = 60000.0
+
+func Calculate(req TaxCalculationRequest) float64 {
+	income := req.TotalIncome - PersonalAllowance
+	if income <= 150000 {
+		return 0
+	} else if income <= 500000 {
+		return (income - 150000) * 0.1
+	}
+
+	return 0
+}
