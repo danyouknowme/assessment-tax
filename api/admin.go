e63c8517b5b0cb206acc7bbf338fc04423a57591
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-27 01:54:45 +0700

    implement admin api to setting persnal deduction

--- api/admin.go
+++ api/admin.go
@@ -1,12 +1,33 @@
 package api
 
 import (
+	"github.com/danyouknowme/assessment-tax/db"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
+type SettingPersonalDeductionRequest struct {
+	Amount float64 `json:"amount"`
+}
+
 func (s *Server) SettingPersonalDeduction(c echo.Context) error {
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "personal deduction set successfully",
+	var req SettingPersonalDeductionRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	deduction, err := s.store.UpdateDeductionByType(
+		c.Request().Context(),
+		"personal",
+		db.UpdateDeductionParams{
+			Amount: req.Amount,
+		},
+	)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	return c.JSON(http.StatusOK, map[string]float64{
+		"personalDeduction": deduction.Amount,
 	})
 }
