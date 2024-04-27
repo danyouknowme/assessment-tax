3c84714ef496994a2c47405f3ca29efec6dbe230
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-27 19:29:15 +0700

    add error tests for setting k-receipt deduction api

--- api/admin_test.go
+++ api/admin_test.go
@@ -56,9 +56,9 @@ func TestAdminSetPersonalDeductionAPI(t *testing.T) {
 			},
 		},
 		{
-			name: "Invalid Body(Negative Amount)",
+			name: "Invalid Body(Amount < 10000)",
 			body: map[string]float64{
-				"amount": -70000.0,
+				"amount": 5000.0,
 			},
 			setupAuth: func(request *http.Request) {
 				request.SetBasicAuth("adminTest", "test!")
@@ -165,6 +165,66 @@ func TestAdminSetKReceiptDeductionAPI(t *testing.T) {
 
 				expected := `{"kReceipt":70000}`
 				require.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(recorder.Body.String()))
+			},
+		},
+		{
+			name: "Invalid Body(Missing Amount)",
+			body: map[string]float64{},
+			setupAuth: func(request *http.Request) {
+				request.SetBasicAuth("adminTest", "test!")
+			},
+			buildStubs: func(store *mockdb.MockStore) {},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "Invalid Body(Negative Amount)",
+			body: map[string]float64{
+				"amount": -70000.0,
+			},
+			setupAuth: func(request *http.Request) {
+				request.SetBasicAuth("adminTest", "test!")
+			},
+			buildStubs: func(store *mockdb.MockStore) {},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "Not Found K-Receipt Deduction",
+			body: map[string]float64{
+				"amount": 70000.0,
+			},
+			setupAuth: func(request *http.Request) {
+				request.SetBasicAuth("adminTest", "test!")
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					UpdateDeductionByType(gomock.Any(), gomock.Any(), db.UpdateDeductionParams{Amount: 70000.0}).
+					Times(1).
+					Return(nil, sql.ErrNoRows)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name: "Failed to Update K-Receipt Deduction",
+			body: map[string]float64{
+				"amount": 70000.0,
+			},
+			setupAuth: func(request *http.Request) {
+				request.SetBasicAuth("adminTest", "test!")
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					UpdateDeductionByType(gomock.Any(), gomock.Any(), db.UpdateDeductionParams{Amount: 70000.0}).
+					Times(1).
+					Return(nil, sql.ErrConnDone)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
 			},
 		},
 	}
