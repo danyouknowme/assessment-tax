aa004baccd0f42cbea0481fa02b2b61cbb63b9f5
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-27 16:19:13 +0700

    implement acceptCSVExtension for except csv file

--- api/middleware.go
+++ api/middleware.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"errors"
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strings"
 )
 
 func (s *Server) basicAuth(next echo.HandlerFunc) echo.HandlerFunc {
@@ -17,6 +19,25 @@ func (s *Server) basicAuth(next echo.HandlerFunc) echo.HandlerFunc {
 		if u != s.config.AdminUsername || p != s.config.AdminPassword {
 			err := errors.New("invalid username or password")
 			return c.JSON(http.StatusUnauthorized, errorResponse(err))
+		}
+
+		return next(c)
+	}
+}
+
+func (s *Server) acceptCSVExtension(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		file, err := c.FormFile("taxFile")
+		if err != nil {
+			err := errors.New("missing file")
+			return c.JSON(http.StatusBadRequest, errorResponse(err))
+		}
+
+		fmt.Println("File Header: ", file.Header.Get("Content-Type"))
+
+		if !strings.HasPrefix(file.Header.Get("Content-Type"), "text/csv") {
+			err := errors.New("invalid file format")
+			return c.JSON(http.StatusBadRequest, errorResponse(err))
 		}
 
 		return next(c)
