978552ca07d572cd98767e65064b2b563435c84c
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-27 16:18:23 +0700

    add tests for accept csv middleware

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,11 +1,20 @@
 package api
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/danyouknowme/assessment-tax/config"
 	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io"
+	"mime"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/textproto"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -63,4 +72,84 @@ func TestBasicAuthMiddleware(t *testing.T) {
 			tc.checkResponse(t, recorder)
 		})
 	}
+}
+
+func TestAcceptCSVExtension(t *testing.T) {
+	testCases := []struct {
+		name          string
+		filePath      string
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:     "OK",
+			filePath: filepath.Join("..", "testdata", "taxes.csv"),
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name:     "Missing File",
+			filePath: "",
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name:     "Invalid File Format",
+			filePath: filepath.Join("..", "testdata", "taxes.txt"),
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			server := NewServer(&config.Config{}, nil)
+
+			server.router.GET("/csv", server.acceptCSVExtension(func(c echo.Context) error {
+				return c.String(http.StatusOK, "OK")
+			}))
+
+			if tc.filePath == "" {
+				recorder := httptest.NewRecorder()
+				request, err := http.NewRequest(http.MethodGet, "/csv", nil)
+				require.NoError(t, err)
+
+				server.router.ServeHTTP(recorder, request)
+				tc.checkResponse(t, recorder)
+				return
+			}
+
+			var file *os.File
+			file, err := os.Open(tc.filePath)
+			assert.NoError(t, err)
+			defer file.Close()
+
+			body := new(bytes.Buffer)
+			writer := multipart.NewWriter(body)
+			part, err := createFormFile(writer, file.Name())
+			require.NoError(t, err)
+
+			_, err = io.Copy(part, file)
+			require.NoError(t, err)
+			require.NoError(t, writer.Close())
+
+			recorder := httptest.NewRecorder()
+			request, err := http.NewRequest(http.MethodGet, "/csv", body)
+			require.NoError(t, err)
+
+			request.Header.Set("Content-Type", writer.FormDataContentType())
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
+
+func createFormFile(w *multipart.Writer, filename string) (io.Writer, error) {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"; filename="%s"`, "taxFile", filename))
+	h.Set("Content-Type", mime.TypeByExtension(filepath.Ext(filename)))
+	return w.CreatePart(h)
 }
