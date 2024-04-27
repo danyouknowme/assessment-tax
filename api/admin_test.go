15c63dbf4ef45e7dca93adf9cf13067e16dd2d46
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-27 19:22:26 +0700

    add success test for setting k-receipt deduction api

--- api/admin_test.go
+++ api/admin_test.go
@@ -137,3 +137,66 @@ func TestAdminSetPersonalDeductionAPI(t *testing.T) {
 		})
 	}
 }
+
+func TestAdminSetKReceiptDeductionAPI(t *testing.T) {
+	testCases := []struct {
+		name          string
+		body          map[string]float64
+		setupAuth     func(request *http.Request)
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			body: map[string]float64{
+				"amount": 70000.0,
+			},
+			setupAuth: func(request *http.Request) {
+				request.SetBasicAuth("adminTest", "test!")
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					UpdateDeductionByType(gomock.Any(), gomock.Any(), db.UpdateDeductionParams{Amount: 70000.0}).
+					Times(1).
+					Return(&db.Deduction{Type: "k-receipt", Amount: 70000.0}, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+
+				expected := `{"kReceipt":70000}`
+				require.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(recorder.Body.String()))
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			cfg := config.Config{
+				AdminUsername: "adminTest",
+				AdminPassword: "test!",
+			}
+
+			server := NewServer(&cfg, store)
+			recorder := httptest.NewRecorder()
+
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+
+			url := "/admin/deductions/k-receipt"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+
+			request.Header.Set("Content-Type", "application/json")
+
+			tc.setupAuth(request)
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
+}
