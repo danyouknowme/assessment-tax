21dc2df77e2e9418f6abdb53965b003750d80a6c
Thanathip Suwannakhot <thanathip.suw@gmail.com>
2024-04-28 18:05:45 +0700

    add function reset db

--- db/db.go
+++ db/db.go
@@ -44,3 +44,26 @@ func PrepareDatabase(db *sql.DB) error {
 
 	return nil
 }
+
+func ResetDatabase(db *sql.DB) error {
+	_, err := db.Exec(`
+		TRUNCATE TABLE "deductions" RESTART IDENTITY CASCADE
+	`)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(`
+		INSERT INTO "deductions" ("type", "amount")
+		VALUES
+			('personal', 60000.00),
+			('donation', 100000.00),
+			('k-receipt', 50000.00)
+		ON CONFLICT (type) DO NOTHING
+	`)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
